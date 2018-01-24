06adda92a989f474867b3f180e67f2eb67f79a95
americanman <snowboardracer7@gmail.com>
2018-01-23 20:47:47 -0500

    SOLVED Quiz using swith statement

---
+++ main.js
@@ -0,0 +1,34 @@
+/*
+ * Programming Quiz: Back to School (3-9)
+ */
+
+// change the value of `education` to test your code
+var education = "no high school diploma";
+
+// set the value of this based on a person's education
+var salary = 25636;
+
+// your code goes here
+ switch (salary) {
+     case 25636:
+        console.log("In 2015, a person with " + education + " earned an average of $" + salary.toLocaleString("en-US") + "/year.");
+     break;
+     case 35256:
+        console.log("In 2015, a person with " + education + " earned an average of $" + salary.toLocaleString("en-US") + "/year.");
+     break;
+     case 41496:
+        console.log("In 2015, a person with " + education + " earned an average of $" + salary.toLocaleString("en-US") + "/year.");
+     break;
+     case 59124:
+        console.log("In 2015, a person with " + education + " earned an average of $" + salary.toLocaleString("en-US") + "/year.");
+     break;
+     case 69732:
+        console.log("In 2015, a person with " + education + " earned an average of $" + salary.toLocaleString("en-US") + "/year.");
+     break;
+     case 89960:
+        console.log("In 2015, a person with " + education + " earned an average of $" + salary.toLocaleString("en-US") + "/year.");
+     break;
+     case 84396:
+        console.log("In 2015, a person with " + education + " earned an average of $" + salary.toLocaleString("en-US") + "/year.");
+     break;
+ }
